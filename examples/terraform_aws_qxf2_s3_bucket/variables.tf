f45912d96eeda8e137db6757b62a3a293c368d39
PreedhiVivek <preedhi.vivek@qxf2.com>
2023-08-16 15:17:58 +0530

    Terraform to create a S3 bucket with versioning and upload multiple files

---
+++ examples/terraform_aws_qxf2_s3_bucket/variables.tf
@@ -0,0 +1,18 @@
+
+variable "AWS_REGION" {
+  description = "AWS region"
+  type        = string
+  default     = "region-name"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key"
+  type        = string
+  default     = "access-key-id"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
+  type        = string
+  default     = "secret-access-key"
+}
