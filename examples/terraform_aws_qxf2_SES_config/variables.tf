9b4b7d482f87ba6c4a8ebcd5b925c98b43ceb4fe
Drishya <drishya.tm@qxf2.com>
2023-08-22 21:32:03 +0530

    added terraform to configure ses

---
+++ examples/terraform_aws_qxf2_SES_config/variables.tf
@@ -0,0 +1,23 @@
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key"
+  type        = string
+  default     = "access-key-id"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
+  type        = string
+  default     = "secret-access-key"
+}
+
+variable "domain_name" {
+  type    = string
+  default = "test-resource-ses.com"
+}
+
+variable "name" {
+  type    = string
+  default = "test-resource-SES"
+}
+
