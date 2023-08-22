7607526587b4a4da893d1a024c0383ea3874e3b3
rajiarju <raji@qxf2.com>
2023-08-22 14:00:24 +0530

    creating aws SQS via terraform infra

---
+++ terraform_aws_qxf2_SQS_config/variables.tf
@@ -0,0 +1,24 @@
+# aws config values
+variable "queue_name" {
+  description = "Name of the SQS queue"
+  type        = string
+  default     = "qxf2-test-queue"
+}
+
+variable "AWS_REGION" {
+  description = "AWS region"
+  type        = string
+  default     = "region-name"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key"
+  type        = string
+  default     = "AWS access key"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
+  type        =  string
+  default     = "AWS secret access key"
+}
