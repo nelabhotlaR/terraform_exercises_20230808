cef66b9e87718d9d2b6b75f50bf700771520eae7
Rohan Dudam <rohan@qxf2.com>
2023-08-23 18:10:20 +0530

    Added script to deploy Qxf2's training website

---
+++ examples/terraform_aws_qxf2_training_website/main.tf
@@ -0,0 +1,48 @@
+# Create an EC2 instance
+resource "aws_instance" "training_wedsite_instance" {
+  ami           = "ami-0261755bbcb8c4a84" # AMI ID
+  instance_type = var.training_website_ec2_inst_type
+  key_name      = var.training_website_key_pair  #key pair name
+  vpc_security_group_ids = [aws_security_group.training_website_sg.id]
+    tags = {
+        "Name"  : "training_website-EC2instance"
+    }
+  provisioner "remote-exec" {
+    connection {
+      host        = aws_instance.training_wedsite_instance.public_ip
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+    }
+    inline = [ 
+     # training website deployment
+      "echo starting setup for the app",
+      "sudo apt-get install -y git",
+      "sleep 2m",
+      "git clone https://github.com/qxf2/training-website.git",
+      "sudo apt-get install -y python3-pip",
+      "sudo apt install -y python3-virtualenv",
+      "cd ~/training-website",
+      "echo Creating a new virtual enviroment",
+      "virtualenv venv-tsqa",
+      "sleep 2s",
+      "echo Activate your virtual environment", 
+      ". venv-tsqa/bin/activate",
+      "sleep 2s",
+      "echo Installing the requirements",
+      "pip install -r requirements.txt",
+      "echo Installing the gunicorn",
+      "pip install gunicorn",
+      "sleep 2s",
+      "echo Running the app",
+      "gunicorn -w 4 -b 0.0.0.0:6464 tsqa:app &",
+      "echo App deployed successfully",
+      "sleep 10s"
+    ]
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install -y python3-virtualenv
+              sudo apt-get install -y python3-pip
+              EOF
+}
