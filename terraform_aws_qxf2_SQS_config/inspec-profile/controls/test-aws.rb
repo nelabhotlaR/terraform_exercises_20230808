f56c0673e90c2ad368ff47d7826a5e3ce5982e15
rajiarju <raji@qxf2.com>
2023-08-30 04:34:45 +0530

    removed smart quotes from terraform.json file

--- terraform_aws_qxf2_SQS_config/inspec-profile/controls/test-aws.rb
+++ terraform_aws_qxf2_SQS_config/inspec-profile/controls/test-aws.rb
@@ -1,16 +1,18 @@
-
 # verify AWS SQS Configuration
 title "aws SQS config test"
 
-Content = inspec.profile.file(“terraform.json”)
-Params = JSON.parse(content)
-sqs = params['queue_name'][value]
+# Load the content of terraform.json
+content = inspec.profile.file("terraform.json")
+params = JSON.parse(content)
+
+# Retrieve the queue_name value
+queue_url = params["queue_name"]["value"]
 
 # you add controls here
 control "aws-sqs-config" do                      
   impact 1.0                               
   title "verify aws sqs configuration"                                       
-  describe aws_sqs(queue_url: attribute(sqs)) do
-    it { should exist}
+  describe aws_sqs_queue(queue_url) do
+    it { should exist }
   end
 end
