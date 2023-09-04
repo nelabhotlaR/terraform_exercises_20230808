5fcb0a4de7a8a7aaa4e5f24127896f79f53358a1
PreedhiVivek <preedhi.vivek@qxf2.com>
2023-09-04 13:31:58 +0530

    Added variable to hold objects path and get AWS credentials from environment

--- examples/terraform_aws_qxf2_s3_bucket/provider.tf
+++ examples/terraform_aws_qxf2_s3_bucket/provider.tf
@@ -8,8 +8,4 @@ terraform {
 }
 
 # Configure the AWS Provider
-provider "aws" {
-  region = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
+provider "aws" {}
--- examples/terraform_aws_qxf2_s3_bucket/variables.tf
+++ examples/terraform_aws_qxf2_s3_bucket/variables.tf
@@ -1,18 +1,5 @@
-
-variable "AWS_REGION" {
-  description = "AWS region"
+variable "OBJECTS_PATH" {
+  description = "Path to the objects directory"
   type        = string
-  default     = "region-name"
-}
-
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS access key"
-  type        = string
-  default     = "access-key-id"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS secret access key"
-  type        = string
-  default     = "secret-access-key"
+  default     = "./objects/"
 }
