02009909b15afbd96055214f95d58cc9d85cf755
Raghava Nelabhotla <raghava.nelabhotla@gmail.com>
2023-09-20 20:16:12 +0530

    added few more attributes to sqs queue

--- examples/terraform_aws_qxf2_SQS_config/main.tf
+++ examples/terraform_aws_qxf2_SQS_config/main.tf
@@ -1,4 +1,12 @@
 # Create sqs queue
 resource "aws_sqs_queue" "queue" {
-  name = var.queue_name
+  name = "${var.queue_name}.fifo"
+  fifo_queue = true
+  delay_seconds = 90
+  max_message_size = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  tags = {
+    Environment = "test"
+  }
 }
