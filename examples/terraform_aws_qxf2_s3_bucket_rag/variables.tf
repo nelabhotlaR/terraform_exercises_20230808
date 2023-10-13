b9d86a418e3747b38af7e2ae062a3ee9d425897d
Raghava Nelabhotla <raghava.nelabhotla@gmail.com>
2023-10-13 11:27:06 +0530

    fixed the issue with s3 bucket

--- examples/terraform_aws_qxf2_s3_bucket_rag/variables.tf
+++ examples/terraform_aws_qxf2_s3_bucket_rag/variables.tf
@@ -1,17 +1,32 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default = "qxf2-bucket"
+  default     = "qxf2-bucket-01"
+}
+
+variable "bucket_environment" {
+  default = "The environment name"
+  type    = string
 }
 
 variable "github_repo" {
   description = "The GitHub repository URL"
   type        = string
-  default = "nelabhotlaR/terraform_exercises_20230808"
+  default     = "nelabhotlaR/terraform_exercises_20230808"
 }
 
 variable "github_file_path" {
   description = "The file path in the GitHub repository"
   type        = string
-  default = "templates/s3_sample.txt"
+  default     = "templates/sample.txt"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "profile" {
+  type    = string
+  default = "personal"
 }
